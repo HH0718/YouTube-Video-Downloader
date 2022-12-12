a4a40cb1855ae7a07e8336c4b7bcb661cc01f70f
James <james.s.miller1@me.com>
2022-12-12 02:14:19 -0800

    pytube_proj.py
    Here's my version. Take look.

--- pytube_proj.py
+++ pytube_proj.py
@@ -1,66 +1,106 @@
+from enum import Enum, auto
+import validators
 from pytube import YouTube
 import ffmpeg
-import os
+from pathlib import Path
+
+DEFAULT_FILTER = {"only_video": True,
+                  "adaptive": True,
+                  "file_extension": "mp4"}
 
 
-def vid_only():
-    for stream in yt.streams.filter(only_video=True, adaptive=True, file_extension="mp4"):
-        res_avail.add((stream.resolution))
-    r = ", ".join(res_avail)
-    print(f"Available resolutions:  {r}")
-
-    res_choose = input("Input your desired resolution: ")
-    print("Downloading . . .")
-    vid_download = yt.streams.filter(res=res_choose.lower()).first().download(filename=yt.title + " VIDEO ONLY.mp4")
+class StreamType(Enum):
+    VIDEO = auto()
+    AUDIO = auto()
+    VIDEO_AUDIO = auto()
 
 
-   
-def aud_only():
-    aud_download = yt.streams.get_audio_only()
-    aud_download.download(filename=yt.title + " AUDIO ONLY.mp4")
+def input_url():
+    valid_url = False
+    while not valid_url:
+        download_url = input("Please enter youtube URL: ")
+        if download_url == "":
+            download_url = "https://www.youtube.com/watch?v=SDpCzJw2xm4&t=1s"
+        valid_url = validators.url(download_url)
+        print("\n")
+        if not valid_url:
+            print("URL is not valid.")
+            print(100 * "\n")
+    return download_url
 
 
-def convert_to_mp3():
-    audio_stream_mp3 = yt.title + " AUDIO ONLY.mp3"
-    video = ffmpeg.input(yt.title + " AUDIO ONLY.mp4")
-    audio = video.audio
-    stream = ffmpeg.output(audio, audio_stream_mp3)
-    ffmpeg.run(stream)
+def input_stream_type() -> StreamType:
+    valid_stream_type = False
 
-    os.remove(yt.title + " AUDIO ONLY.mp4")
+    while not valid_stream_type:
+        [print(f"{stream_type.name} = {stream_type.value}")
+         for stream_type in list(StreamType)]
 
-def vid_and_aud():
-    vid = ffmpeg.input(yt.title + " VIDEO ONLY.mp4")
-    aud = ffmpeg.input(yt.title + " AUDIO ONLY.mp4")
+        try:
+            stream_type = StreamType(
+                int(input("Please enter Stream Type number: ")))
+            valid_stream_type = True
+            print("\n")
+
+        except (ValueError, IndexError):
+            print("Please enter a valid entry")
+            print(100 * "\n")
+
+    return stream_type
 
 
-    ffmpeg.output(aud, vid, 'out.mp4').run()
+def download_video(youtube: YouTube):
+    streams = [stream for stream in sorted(youtube.streams.filter(
+        **DEFAULT_FILTER), key=lambda video: video.resolution[-1]) if stream.video_codec.startswith('avc1')]
 
-    os.remove(yt.title + " AUDIO ONLY.mp4")
-    os.remove(yt.title + " VIDEO ONLY.mp4")
+    [print(i, stream.resolution) for i, stream in enumerate(streams, start=1)]
 
+    selected_resolution = int(
+        input('Please enter the resolution you want to download i.e "1": '))
 
+    streams[selected_resolution - 1].download(filename=f"VIDEO_{youtube.title}.mp4")
 
-link = input("Input youtube link: ")
-yt = YouTube(link)
-res_avail = set()
-stream_type = int(input("Types: \n1. Video only \n2. Audio only \n3. Video with audio \nEnter the number of the type you want to download: "))
+def download_audio(youtube: YouTube):
+    youtube.streams.get_audio_only().download(filename=f"AUDIO_{youtube.title}.mp4")
 
-if stream_type == 1:
-    vid_only()
-   
+def post_process(youtube: YouTube):
+    video = ffmpeg.input(filename=f"VIDEO_{youtube.title}.mp4")
+    audio = ffmpeg.input(filename=f"AUDIO_{youtube.title}.mp4")
 
-elif stream_type == 2:
-    aud_only()
-    convert_to_mp3()
+    ffmpeg.output(audio, video, f'./{youtube.title}.mp4').run()
 
-elif stream_type == 3:
-    vid_only()
-    aud_only()
-    vid_and_aud()
+    Path(f"VIDEO_{youtube.title}.mp4").unlink()
+    Path(f"AUDIO_{youtube.title}.mp4").unlink()
     
-else:
-    print("Invalid input")
 
 
-print(yt.title + "\nHas been successfully downloaded.")
+def download_video_audio(youtube: YouTube):
+    download_video(youtube)
+    download_audio(youtube)
+    post_process(youtube)
+
+
+def download_init(url: str, stream_type: StreamType):
+    youtube = YouTube(url)
+    match stream_type:
+        case StreamType.VIDEO:
+            download_video(youtube)
+
+        case StreamType.AUDIO:
+            download_audio(youtube)
+
+        case StreamType.VIDEO_AUDIO:
+            download_video_audio(youtube)
+
+
+
+def main() -> None:
+    download_url = input_url()
+    stream_type = input_stream_type()
+
+    print(f"You selected {stream_type.name} download from {download_url}")
+    download_init(download_url, stream_type)
+
+
+if __name__ == "__main__":
+    main()
